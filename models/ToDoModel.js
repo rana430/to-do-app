5c8c128192b872c7f11af9631aa97dd372fdc453
Rana Wahid <ranawahid34@gmail.com>
2023-09-15 01:14:12 +0300

    backend

---
+++ models/ToDoModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const toDoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  description: { type: String },
+  dueDate: { type: Date },
+  creationDate: {
+    type: Date,
+    default: Date.now,
+  },
+  status: { type: String, default: "pending" },
+  isLate: { type: Boolean, defult: false },
+});
+
+module.exports = mongoose.model("ToDo", toDoSchema);
